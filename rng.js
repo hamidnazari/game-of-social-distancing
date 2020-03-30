2007bc9196017b74e829dc89c1bc36ad88e60c95
Hamid Nazari <hn@linux.com>
2020-03-31 01:32:18 +1100

    Fixed error in RNG

--- rng.js
+++ rng.js
@@ -1,17 +1,17 @@
 var RNG = (function() {
   const SEED = 0;
 
-  let rng = new Math.seedrandom(SEED);
+  const rng = new Math.seedrandom(SEED);
 
   return {
     random: rng,
     randNumber: function(min, max) {
-      return Math.random() * (max - min) + min;
+      return rng() * (max - min) + min;
     },
     randInteger: function(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
+      return Math.floor(rng() * (max - min)) + min;
     },
     randFalse: function(weight=0.5) {
       return rng() >= weight;
